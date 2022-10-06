b2eb97ee16ef4c48ac4d85aabf5b0c3af7dbd4c2
Adam Rosas <adamrosas27@gmail.com>
2022-10-06 16:48:05 -0700

    Create constructor method
    Create constructor method with parameters name, level, max_health, current_health, and knocked_out will be basis for future created characters

--- poke.py
+++ poke.py
@@ -1,1 +1,9 @@
 class Character:
+
+    # Create constructor method:
+    def __init__(self, name, level, max_health, current_health, knocked_out):
+        self.name = name
+        self.level = level
+        self.max_health = max_health
+        self.current_health = current_health
+        self.knocked_out = knocked_out
