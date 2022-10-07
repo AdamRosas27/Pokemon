f75a26735e937bf112e653639c5d858113ea651f
Adam Rosas <adamrosas27@gmail.com>
2022-10-06 17:07:11 -0700

    Update constructor method
    Add assertion that name must be at least greater than two characters and default values for parameters

--- poke.py
+++ poke.py
@@ -1,7 +1,11 @@
 class Character:
 
     # Create constructor method:
-    def __init__(self, name, level, max_health, current_health, knocked_out):
+    def __init__(self, name, level=0, max_health=100, current_health=100, knocked_out=False):
+        # Make sure the name given is a valid length
+        assert len(
+            name) > 1, f"Name {name} is too short. Name cannot be less than 2 characters."
+
         self.name = name
         self.level = level
         self.max_health = max_health
